6f9e257f00022f7e444d30ad5713254083a3d21f
kant1724 <sonseungbum83@gmail.com>
2019-06-30 10:55:52 +0900

    commit

--- Titanic/train_MSE.py
+++ Titanic/train_MSE.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import RobustScaler
 import torch
 import torch.nn as nn
 
-input_size = 6
+input_size = 34
 num_classes = 1
 num_epochs = 10000
 batch_size = 100
@@ -19,25 +19,42 @@ for tt in train['Survived']:
     y.append([tt])
 y = np.array(y, dtype=np.long)
 all = pd.concat([train, test], ignore_index=True, sort=False)
-all = all.loc[:, ['PassengerId', 'Pclass', 'SibSp', 'Parch', 'Sex', 'Age', 'Fare']]
+all = all.loc[:, ['PassengerId', 'Pclass', 'SibSp', 'Parch', 'Sex', 'Age', 'Fare', 'Embarked', 'Name']]
 all['Age'] = all['Age'].fillna(20)
-all['Age'] = all['Age'] / 10
+a = all['Age']
+for i in range(len(a)):
+    if a[i] <= 10:
+        a[i] = 'a'
+    elif a[i] <= 20:
+        a[i] = 'b'
+    elif a[i] <= 30:
+        a[i] = 'c'
+    elif a[i] <= 40:
+        a[i] = 'd'
+    elif a[i] <= 50:
+        a[i] = 'e'
+    elif a[i] <= 60:
+        a[i] = 'f'
+    else:
+        a[i] = 'g'
+
+a = all['Name']
+for i in range(len(a)):
+    a[i] = a[i].split(', ')[1].split('. ')[0]
+
 all['Fare'] = all['Fare'] / 100
 
-a = all['Sex']
-for i in range(len(a)):
-    if a[i] == 'male':
-        a[i] = 0.5
-    else:
-        a[i] = 1
+all.to_csv('./table.csv')
 
-all.to_csv('./dd.csv')
+all = pd.get_dummies(all)
 
 train_len = train.shape[0]
 train = all[:train_len]
 test = all[train_len:]
 train = train.drop('PassengerId', axis=1)
 test = test.drop('PassengerId', axis=1)
+
+sc = RobustScaler()
 
 # Logistic regression model
 model = nn.Linear(input_size, num_classes)
@@ -46,6 +63,7 @@ criterion = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 x = train.to_numpy()
+#x = sc.fit_transform(x)
 
 # Train the model
 for epoch in range(num_epochs):
@@ -61,10 +79,12 @@ for epoch in range(num_epochs):
     optimizer.step()
 
 x = test.to_numpy()
+#x = sc.transform(x)
+
 predicted = model(torch.from_numpy(x.astype(np.float32))).detach().numpy()
 preds = []
 for p in predicted:
-    if p[0] > 0.3:
+    if p[0] > 0.5:
         preds.append(1)
     else:
         preds.append(0)
